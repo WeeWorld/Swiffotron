b2890ba7fcee124aa99f87ce7e9b411624ddb031
Ian Beveridge <ian.beveridge@gmail.com>
2012-05-24 00:45:33 +0100

    Update express solution slightly. Also add explode option to CLI

---
+++ SwiffotronCLI/ABCCatcher.cs
@@ -0,0 +1,40 @@
+﻿//-----------------------------------------------------------------------
+// ABCCatcher.cs
+//
+//
+//-----------------------------------------------------------------------
+
+
+namespace SwiffotronCLI
+{
+    using SWFProcessing.SWFModeller.ABC.Debug;
+    using System.Collections.Generic;
+    using System.IO;
+
+    class ABCCatcher : IABCLoadInterceptor
+    {
+        private Dictionary<string, byte[]> abcFiles;
+
+        public ABCCatcher()
+        {
+            this.abcFiles = new Dictionary<string, byte[]>();
+        }
+
+        public void OnLoadAbc(bool lazyInit, SWFProcessing.SWFModeller.Process.SWFContext ctx, string abcName, int doAbcCount, byte[] bytecode)
+        {
+            abcFiles.Add(abcName + "." + doAbcCount + ".abc", bytecode);
+        }
+
+        public void SaveAll(string swfName, string dir)
+        {
+            foreach (string name in abcFiles.Keys)
+            {
+                using (FileStream fsout = new FileStream(dir + @"\" + swfName + name, FileMode.Create, FileAccess.Write))
+                {
+                    byte[] data = abcFiles[name];
+                    fsout.Write(data, 0, data.Length);
+                }
+            }
+        }
+    }
+}
